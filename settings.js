bdbf0bf053947e293b0fe71f03e24ceaa6bc1547
Raya2399 <102332034+Raya2399@users.noreply.github.com>
2023-03-25 21:00:39 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ const {
    spanyol
 } = require(`./language`)
 
-global.OpenAIKey = "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" //login to https://platform.openai.com/account/api-keys to get apikey
+global.OpenAIKey = "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" //login to https://platform.openai.com/account/api-keys to get apikey
 // Website Api (jgn di ganti tomlol)
 global.APIs = {
    alfa: 'https://api.zeeoneofc.my.id', //apabila link api eror, segera laporkan ke owner
